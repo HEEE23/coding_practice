6be589661965c7f2c8f4df100b40d40e7fb8ab24
HEEE23 <gmltjs1592@gmail.com>
2024-03-15 21:39:27 +0900

    first commit

---
+++ hide_and_seek.py
@@ -0,0 +1,23 @@
+# [백준 : 1697번] 숨바꼭질
+from collections import deque
+n, k = map(int, input().split())
+
+MAX = 10**5
+dist = [0]*(MAX+1)
+def bfs():
+    queue = deque()
+    queue.append(n)
+
+    while queue:
+        popV = queue.popleft()
+
+        if popV == k:
+            print(dist[popV])
+            break
+
+        for nextN in (popV+1, popV-1, 2*popV):
+            if 0<=nextN<=MAX and not dist[nextN]:
+                dist[nextN] = dist[popV] + 1
+                queue.append(nextN)
+
+bfs()
