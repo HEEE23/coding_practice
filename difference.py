fd1f5b9e2a1317ae0bd2775afff5e5b853117e94
HEEE23 <gmltjs1592@gmail.com>
2024-03-21 00:09:53 +0900

    binary_search

---
+++ difference.py
@@ -0,0 +1,38 @@
+# [백준 : 1822번] 차집합
+# 이진탐색
+def binary_search(arr, target, start, end):
+    while start<=end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return None
+
+# 집합 A, B의 원소의 개수
+a, b = map(int, input().split())
+arr_a = list(map(int, input().split())) # 집합 A의 원소
+arr_b = list(map(int, input().split())) # 집합 B의 원소
+
+arr_b.sort()
+
+count = 0
+result = []
+for i in range(a):
+    if binary_search(arr_b, arr_a[i], 0, b-1) == None:
+        count += 1
+        result.append(arr_a[i])
+
+result.sort() # 오름차순 정렬
+# 집합 A에는 속하면서 집합 B에는 속하지 않는 원소가 없다면 0 출력
+if len(result) == 0:
+    print("0")
+else:
+    # 집합 A에는 속하면서 집합 B에는 속하지 않는 원소의 개수 출력
+    print(count)
+    # 구체적인 원소
+    for i in range(len(result)):
+        print(result[i], end = ' ')
---
+++ number_card.py
@@ -0,0 +1,30 @@
+# [백준 : 10815번] 숫자 카드
+# 이진 탐색
+def binary_search(arr, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+# 상근이가 가지고 있는 숫자 카드의 개수
+n = int(input())
+# 숫자 카드에 적혀있는 정수
+num = list(map(int, input().split()))
+# 비교할 카드 개수
+m = int(input())
+# 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 정수
+card = list(map(int, input().split()))
+
+num.sort() # 왜 필요할까
+
+# 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1, 아니면 0
+for i in range(m):
+    if binary_search(num, card[i], 0, n - 1) == None:
+        print("0", end = ' ')
+    else:
+        print("1", end = ' ')
