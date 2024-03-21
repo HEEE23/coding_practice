3ce045bdf266cbd184332529ba95be267a9f4aa3
HEEE23 <gmltjs1592@gmail.com>
2024-03-21 23:43:52 +0900

    binary_search

---
+++ snack.py
@@ -0,0 +1,28 @@
+# [백준 : 16401번] 과자 나눠주기
+# 이진 탐색
+# M : 조카의 수, N : 과자의 수
+m, n = map(int, input().split())
+# 과자의 길이
+snack = list(map(int, input().split()))
+
+start = 1 # 과자의 최소 길이
+end = max(snack) # 주어진 과자의 길이 중 최대값
+
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start+end) // 2
+    # 모든 조카에게 같은 길이의 막대과자를 나눠줄 수 없다면, 0 츌력
+    if mid == 0:
+        break
+
+    for x in snack:
+        if x >= mid:
+            total += (x // mid)
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
