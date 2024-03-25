dd49e08eb0001746815cc75a6112852a307168b1
HEEE23 <128252996+HEEE23@users.noreply.github.com>
2024-03-25 16:05:55 +0900

    BinarySearch

--- BinarySearch/BOJ_18870.py
+++ BinarySearch/BOJ_18870.py
@@ -1,6 +1,6 @@
 # [백준 : 18870번] 좌표 압축
 n = int(input()) # 좌표의 수
-X = list(map(int, input().split())) # n개의 좌표
+X = list(map(int, input().split()))
 
 # X의 값을 중복 제거한 후 리스트로 만들고 정렬
 X_sort = sorted(list(set(X)))
