8c8e8a7663564b96157247ab3090c20038d5aacd
HEEE23 <gmltjs1592@gmail.com>
2024-03-25 15:22:57 +0900

    binary_search

---
+++ coordinate_compression.py
@@ -0,0 +1,14 @@
+# [백준 : 18870번] 좌표 압축
+n = int(input()) # 좌표의 수
+X = list(map(int, input().split())) # n개의 좌표
+
+# X의 값을 중복 제거한 후 리스트로 만들고 정렬
+X_sort = sorted(list(set(X)))
+
+# X_sort를 이용해 딕셔너리 구성
+dic = {}
+for i in range(len(X_sort)):
+    dic[X_sort[i]] = i
+
+for i in X:
+    print(dic[i], end = ' ')
